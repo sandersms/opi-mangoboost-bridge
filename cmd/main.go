d303cfe5ef3165d29d5c45ab72dfe0148750d9ae
Mark Sanders <marksanders194@gmail.com>
2025-01-16 13:50:59 -0600

    ci: add framework files and workflows
    Signed-off-by: Mark Sanders <marksanders194@gmail.com>

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+// Copyright (C) 2023 Nordix Foundation.
+
+// Just a simple main function for now
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("opi-mangoboost-bridge")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/opiproject/opi-mangoboost-bridge
+
+go 1.22.0
